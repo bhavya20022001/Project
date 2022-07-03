20eb3d182018feb13367bc872c7751b4814444c6
Bhavya Surana <surana.bhavya01@gmail.com>
2022-07-03 18:43:19 +0530

    added a sublist containing mood,total episodes and series/drama

--- automating_decision_making.py
+++ automating_decision_making.py
@@ -1,15 +1,14 @@
 import random
-ls=["Strong woman Do Bong Soon","The Sound of Magic","Hotel Del Luna","Business Proposal",
-"Twenty Five Twenty One","Welcome to Wakiki","Fight for my way","Itaewon classes"]
-
+ls=[["Strong woman Do Bong Soon","gives strength","16 episodes","series"],["The Sound of Magic","escape reality","6 episodes","short series"],["Hotel Del Luna","try something new","16 episodes","drama"],["Business Proposal","romantic","16 episodes","drama"],
+["Welcome to Wakiki","jolly and comedy","20 episodes","series"],["Fight for my way","real and inspiring","16 episodes","drama"]]
 i=0
 x=input("Do you want me to pick a drama for you? yes/no : ").lower()
+
 if x== "yes":
 
         i=random.randint(0,len(ls))
         print(ls[i])
-
-if x!="yes":
+else :
     print("maybe next time then !")
 
 
