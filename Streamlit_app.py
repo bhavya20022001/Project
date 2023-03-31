ec8771a152440b6c3a85556bdaebd84a1c08f978
Bhavya Surana <72392439+bhavya20022001@users.noreply.github.com>
2023-03-31 14:07:39 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 # Set the title of your app
-st.title("My demo App1")
+st.title("My demo app1")
 
 # Add a button that prints a message when clicked
 if st.button("Click me!"):
