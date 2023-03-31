2c79ab520dd2753bfcd1c97f007b2895bf924aa5
Bhavya Surana <72392439+bhavya20022001@users.noreply.github.com>
2023-03-31 13:54:37 +0530

    Create Streamlit_app.py

---
+++ Streamlit_app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+# Set the title of your app
+st.title("My demo App1")
+
+# Add a button that prints a message when clicked
+if st.button("Click me!"):
+    st.write("Hello, world!")
+    
